af1f7da2811e82ab040f526ff739303add58513d
Jitse-Jan van Waterschoot <j.waterschoot@marketinvoice.com>
2018-05-17 14:44:28 +0100

    Addec Ocado view

--- uk_groceries/views.py
+++ uk_groceries/views.py
@@ -1,6 +1,17 @@
 from pyramid.view import view_config
 
+OCADO = {
+    'mutti': {
+        'name': 'Mutti',
+        'description': 'Tomatoes'
+    },
+    'rice dream': {
+        'name': 'Rice Dream',
+        'description': 'Milk'
+    }
+}
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
-def my_view(request):
-    return {'project': 'uk-groceries'}
+@view_config(route_name='ocado', renderer='json')
+def get_ocado(request):
+    query = request.matchdict['query']
+    return OCADO[query]
