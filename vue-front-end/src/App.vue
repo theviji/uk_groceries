98132280da17ecf7b3e27147eb1f8bd4e64015b3
Jitse-Jan van Waterschoot <j.waterschoot@marketinvoice.com>
2018-05-21 16:52:05 +0100

    Removed Bulma. Added Vuetify

--- vue-front-end/src/App.vue
+++ vue-front-end/src/App.vue
@@ -1,50 +1,83 @@
 <template>
-  <div id="app">
-    <HelloWorld/>
-  </div>
+  <v-app light>
+    <v-container fluid>
+      <v-toolbar dense>
+        <v-text-field prepend-icon="search"
+                      v-model="searchText"
+                      hide-details single-line></v-text-field>
+        <v-btn @click="sendData" class="primary">submit</v-btn>                    
+      </v-toolbar>
+      <v-layout row>
+        <v-flex xs6 order-rg2>
+          <v-layout row>
+            <v-flex d-flex>
+              <v-layout row wrap>
+                <v-flex v-for='(item, index) in response.items' :key='index' d-flex xs6>
+                  <v-layout card row>
+                    <v-flex xs7>
+                      <div>
+                        <div class="headline">{{ item.name }}</div>
+                        <div>GBP {{ item.price.currentPrice.replace('&poun', '').replace('d;', '') }}</div>
+                      </div>
+                    </v-flex>
+                    <v-flex xs5>
+                      <a :href="item.simplifiedBopUrl">
+                        <v-card-media
+                          :src="item.imageSrc"
+                          height="125px"
+                          contain
+                        ></v-card-media>
+                      </a>
+                    </v-flex>
+                  </v-layout>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex xs6>
+          <v-card dark tile flat color="red darken-4">
+            <v-card-text>#2</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data () {
+      return {
+          searchText: "",
+          response: ""
+      }
+  },
+  methods: {
+      sendData() {
+          axios({ method: "GET", "url": "http://localhost:6543/ocado/"+ this.searchText, "headers": { "content-type": "application/json" } }).then(result => {
+              console.log(result.data);
+              this.response = result.data;
+          }, error => {
+              console.error(error);
+          });
+      }
   }
 }
 </script>
 
-<style lang="scss">
-/* Import Bulma's core */
-@import "~bulma/sass/utilities/_all";
-
-/* Set your colors */
-$primary: #8c67ef;
-$primary-invert: findColorInvert($primary);
-$twitter: #4099FF;
-$twitter-invert: findColorInvert($twitter);
-
-/* Setup $colors to use as bulma classes (e.g. 'is-twitter') */
-$colors: (
-    "white": ($white, $black),
-    "black": ($black, $white),
-    "light": ($light, $light-invert),
-    "dark": ($dark, $dark-invert),
-    "primary": ($primary, $primary-invert),
-    "info": ($info, $info-invert),
-    "success": ($success, $success-invert),
-    "warning": ($warning, $warning-invert),
-    "danger": ($danger, $danger-invert),
-    "twitter": ($twitter, $twitter-invert)
-);
-
-/* Links */
-$link: $primary;
-$link-invert: $primary-invert;
-$link-focus-border: $primary;
-
-/* Import Bulma and Buefy styles */
-@import "~bulma";
-@import "~buefy/src/scss/buefy";
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
 </style>
--- vue-front-end/src/main.js
+++ vue-front-end/src/main.js
@@ -1,11 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Buefy from 'buefy'
-import 'buefy/lib/buefy.css'
+import Vuetify from 'vuetify'
 import App from './App'
 
-Vue.use(Buefy)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#3f51b5',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#b71c1c'
+  }
+})
+import 'vuetify/dist/vuetify.min.css' 
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
